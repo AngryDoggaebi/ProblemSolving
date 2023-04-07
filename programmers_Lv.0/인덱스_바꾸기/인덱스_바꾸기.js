b5ae5476bf22502ffb2567bd419e7ca496e01d2c
AngryDoggaebi <120698922+AngryDoggaebi@users.noreply.github.com>
2023-04-07 23:05:32 +0900

    Create 인덱스_바꾸기.js

---
+++ programmers_Lv.0/인덱스_바꾸기/인덱스_바꾸기.js
@@ -0,0 +1,8 @@
+function solution(my_string, num1, num2) {
+    const str1 = my_string[num1];
+    const str2 = my_string[num2];
+    my_string = my_string.split("");
+    my_string.splice(num1,1,str2);
+    my_string.splice(num2,1,str1);
+    return my_string.join("")
+}
