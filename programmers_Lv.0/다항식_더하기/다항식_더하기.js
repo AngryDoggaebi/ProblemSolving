fe72aa037496b23e93c4df89d6917483d7d70fba
AngryDoggaebi <120698922+AngryDoggaebi@users.noreply.github.com>
2023-04-14 23:02:00 +0900

    Create 다항식_더하기.js

---
+++ programmers_Lv.0/다항식_더하기/다항식_더하기.js
@@ -0,0 +1,30 @@
+function solution(polynomial) {
+    let newPoly = polynomial.split(' + ');
+    let xNum = 0;
+    let num = 0;
+    let result = '';
+    
+    for(let i=0; i<newPoly.length; i++){
+        //xNum
+        if(newPoly[i].includes('x')){
+            if(newPoly[i] === 'x') xNum++;
+            else xNum += Number(newPoly[i].replace('x',""));
+        //num
+        } else {
+            num += Number(newPoly[i]);
+        }
+    }
+    
+    if(xNum === 1){
+        if(num === 0) result = `x`;
+        else if(xNum === 0) result = `${num}`;
+        else result = `x + ${num}`;
+    } else {
+        if(num === 0) result = `${xNum}x`;
+        else if(xNum === 0) result = `${num}`;
+        else result = `${xNum}x + ${num}`;
+    }
+    
+    return result
+    
+}
