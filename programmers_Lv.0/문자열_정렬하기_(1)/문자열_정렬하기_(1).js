59b3b27d7992a8c9edd7e4edea0003b74443aede
AngryDoggaebi <120698922+AngryDoggaebi@users.noreply.github.com>
2023-04-24 20:44:57 +0900

    Update 문자열_정렬하기_(1).js

--- programmers_Lv.0/문자열_정렬하기_(1)/문자열_정렬하기_(1).js
+++ programmers_Lv.0/문자열_정렬하기_(1)/문자열_정렬하기_(1).js
@@ -1,9 +1,7 @@
 function solution(my_string) {
-    let isNum = /[0-9]/g;
-    let arr = my_string
+    return my_string
         .split("")
         .filter( v => v==='0' || +v )
         .map(v => Number(v))
         .sort((a,b) => a-b)
-    return arr
 }
